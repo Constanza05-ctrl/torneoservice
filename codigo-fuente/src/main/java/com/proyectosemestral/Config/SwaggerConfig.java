776aae152071f2f257bf5cab4732d85b57113691
Constanza Carrasco <Cons.carrascoc@duocuc.cl>
2026-06-15 00:59:52 +0000

    doc: Adición de dependencia OpenApi y SwaggerConfig

---
+++ codigo-fuente/src/main/java/com/proyectosemestral/Config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.proyectosemestral.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Torneo API")
+                        .version("1.0")
+                        .description("API para la gestión de torneos en tienda"));
+    }
+}
